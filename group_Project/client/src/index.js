72b139e18d79f37c8a4e775b2b423d70c6e134f8
trananhphuong3008 <75055731+trananhphuong3008@users.noreply.github.com>
2022-04-03 00:00:15 +0700

    Revert "Conect-DB"

--- group_Project/client/src/index.js
+++ group_Project/client/src/index.js
@@ -11,10 +11,6 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById('root')
 )
-//test 
-
-
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
